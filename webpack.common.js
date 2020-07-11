70ec896cd4f382af4db401943ff10008ddba3006
Arwa Alhazmi <arwaalhazmi-is@hotmail.com>
2020-07-11 04:06:42 +0300

    Initial Files and Webpack Config

---
+++ webpack.common.js
@@ -0,0 +1,41 @@
+const path = require('path');
+var HtmlWebpackPlugin = require('html-webpack-plugin');
+
+module.exports = {
+    entry: "./src/client/index.js",
+    module: {
+        rules: [
+            {
+                test: /\.js$/,
+                exclude: /node_modules/,
+                use: {
+                    loader: "babel-loader",
+                }
+            },
+            {
+                test: /\.(png|jpe?g|gif)$/,
+                use:{
+                    loader:"file-loader",
+                    options:{
+                        name: "[name].[ext]",
+                        outputPath: "assets/imgs"
+                    }
+                }
+            },
+            {
+                test: /\.(svg|eot|woff|woff2|ttf)$/,
+                loader: 'file-loader',
+                options: {
+                    name: "[name].[ext]",
+                    outputPath: 'assets/fonts'
+                }
+            }
+        ]
+    },
+    plugins: [
+        new HtmlWebpackPlugin({
+            template: "./src/client/views/index.html",
+            filename: "./index.html"
+        })
+    ]
+};
---
+++ webpack.prod.js
@@ -0,0 +1,30 @@
+const path = require('path');
+const common = require('./webpack.common');
+const merge = require('webpack-merge');
+const { CleanWebpackPlugin } = require('clean-webpack-plugin');
+const MiniCssExtractPlugin = require('mini-css-extract-plugin');
+//const OptimizeCssAssetsPlugin = require('optimize-css-assets-webpack-plugin');
+
+module.exports = merge(common, {
+    mode: "production",
+    module: {
+    	rules: [
+    		{
+                test: /\.scss$/,
+                use: [
+                    MiniCssExtractPlugin.loader,
+                    "css-loader",
+                    "sass-loader"
+                ]
+            }
+    	]
+    },
+    output: {
+        path: path.resolve(__dirname,"dist"),
+        filename: "main.[contentHash].js"
+    },
+    plugins: [
+    	new MiniCssExtractPlugin({filename: "[name].[contentHash].css"}),
+     	new CleanWebpackPlugin()
+     ]
+});
