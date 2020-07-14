38e8bdf0637aa7eb5e389b1c7386dc12ce298210
Arwa Alhazmi <arwaalhazmi-is@hotmail.com>
2020-07-15 02:38:47 +0300

    Setting up page layout

--- src/client/index.js
+++ src/client/index.js
@@ -1,2 +1,6 @@
-import "./styles/main.scss";
+import './styles/main.scss';
 import '@fortawesome/fontawesome-free/js/all.js';
+import './assets/imgs/holder.png';
+import './assets/imgs/london.jpg';
+import './assets/imgs/ocean.jpg';
+import './js/app.js';
